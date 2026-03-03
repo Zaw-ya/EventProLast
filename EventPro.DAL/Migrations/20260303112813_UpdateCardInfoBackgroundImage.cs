51d13a06a13486cbf254bed90191c7c679a45baf
Ali hani <104443638+alihani2002@users.noreply.github.com>
2026-03-03 14:23:43 +0200

    Update cardInfo

---
+++ EventPro.DAL/Migrations/20260303112813_UpdateCardInfoBackgroundImage.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventPro.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCardInfoBackgroundImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BackgroundImage",
+                table: "CardInfo",
+                type: "nvarchar(max)",
+                unicode: false,
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)",
+                oldUnicode: false,
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BackgroundImage",
+                table: "CardInfo",
+                type: "varchar(200)",
+                unicode: false,
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldUnicode: false,
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
---
+++ EventPro.DAL/Migrations/20260303113242_UpdateCardInfoBarcodeColorCode.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventPro.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCardInfoBarcodeColorCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BarcodeColorCode",
+                table: "CardInfo",
+                type: "nvarchar(max)",
+                unicode: false,
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)",
+                oldUnicode: false,
+                oldMaxLength: 150,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BarcodeColorCode",
+                table: "CardInfo",
+                type: "varchar(150)",
+                unicode: false,
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldUnicode: false,
+                oldMaxLength: 150,
+                oldNullable: true);
+        }
+    }
+}
