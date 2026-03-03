9400208abdb92bb080a4b7f4450f0839538a9a61
Ahmed Elghrabawy <adff.free@gmail.com>
2026-03-03 02:29:12 +0200

    added an migration and update the database for the qr settings bg color

---
+++ EventPro.DAL/Migrations/20260303002608_MakeColorsMaxLength.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventPro.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeColorsMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ForegroundColor",
+                table: "CardInfo",
+                type: "nvarchar(max)",
+                unicode: false,
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(40)",
+                oldUnicode: false,
+                oldMaxLength: 40,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BackgroundColor",
+                table: "CardInfo",
+                type: "nvarchar(max)",
+                unicode: false,
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(40)",
+                oldUnicode: false,
+                oldMaxLength: 40,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ForegroundColor",
+                table: "CardInfo",
+                type: "varchar(40)",
+                unicode: false,
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldUnicode: false,
+                oldMaxLength: 40,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BackgroundColor",
+                table: "CardInfo",
+                type: "varchar(40)",
+                unicode: false,
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldUnicode: false,
+                oldMaxLength: 40,
+                oldNullable: true);
+        }
+    }
+}
